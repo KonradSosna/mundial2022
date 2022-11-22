d1eb82bcd7ef628466939cb0aae53dd4b77aadb5
Konrad Sosna <konrad.sosna.95@gmail.com>
2022-11-22 16:57:19 +0100

    change betting layout

--- src/Components/ObstawMecz/index.tsx
+++ src/Components/ObstawMecz/index.tsx
@@ -1,4 +1,10 @@
-import { Grid, TextField, Typography } from '@mui/material';
+import {
+	CircularProgress,
+	Divider,
+	Grid,
+	TextField,
+	Typography,
+} from '@mui/material';
 import { memo, useEffect, useState } from 'react';
 import Container from '../LandingPage/Partials/Container';
 import FormButton from '../LandingPage/Partials/Button';
@@ -14,11 +20,13 @@ import {
 } from 'firebase/firestore';
 import { db, authz } from '../../App';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { isAfter } from 'date-fns';
 
 function ObstawMecz() {
 	const [edit, setEdit] = useState(false);
 	const [index, setIndex] = useState(0);
 	const [bets, setBets] = useState<DocumentData[]>();
+	const [loading, setLoading] = useState(false);
 
 	const user = useAuthState(authz);
 	const { register, handleSubmit } = useForm();
@@ -30,7 +38,7 @@ function ObstawMecz() {
 			leftTeam: data.leftScore,
 			rightTeam: data.rightScore,
 		};
-		console.log(bet);
+		// console.log(bet);
 
 		await setDoc(
 			doc(db, 'bets', `${user[0]?.displayName} ${matchId.toString()}`),
@@ -43,12 +51,15 @@ function ObstawMecz() {
 	};
 
 	async function getCities() {
-		const citiesCol = collection(db, 'bets');
+		await setLoading(true);
+		const betsCol = collection(db, 'bets');
 
-		const citySnapshot = await getDocs(citiesCol);
-		const cityList = citySnapshot.docs.map((doc) => doc.data());
+		const betsSnapshot = await getDocs(betsCol);
+		const betsList = betsSnapshot.docs.map((doc) => doc.data());
 
-		return cityList;
+		await setLoading(false);
+
+		return betsList;
 	}
 
 	useEffect(() => {
@@ -61,13 +72,10 @@ function ObstawMecz() {
 			});
 	}, []);
 
-	console.log(bets)
-
 	return (
 		<>
 			<Container
 				direction="column"
-				devider
 				sx={{
 					justifyContent: 'center',
 					minHeight: '200px',
@@ -86,101 +94,133 @@ function ObstawMecz() {
 			</Container>
 			<Container
 				direction="column"
-				devider
 				sx={{ justifyContent: 'center', marginTop: '30px' }}
 			>
-				<Grid container direction="column" rowGap={6} alignItems="center">
-					{matches.map((match) => (
-						<form
-							key={match.id}
-							onSubmit={handleSubmit((data) => submitForm(data, match.id))}
-						>
+				<Grid container direction="column" rowGap={6}>
+					{matches.map((matchGroup) => (
+						<>
+							<Divider />
 							<Grid
 								container
-								justifyContent="center"
-								alignItems="center"
-								sx={{
-									border: '1px solid black',
-									width: '500px',
-									height: '80px',
-								}}
 								direction="row"
-								spacing={2}
+								alignItems="center"
+								justifyContent="space-evenly"
 							>
-								<Grid item display="flex" alignItems="center">
-									<Typography>{match.leftTeam}</Typography>
-									<Flag
-										code={match.leftFlag}
-										fallback={<span>Unknown</span>}
-										height="16"
-										style={{ margin: '10px' }}
-									/>
-									{edit && index === match.id ? (
-										<TextField
-											{...register('leftScore', { required: true })}
-											style={{ width: '60px' }}
-											type="number"
-											inputProps={{ min: 0, max: 20 }}
-											defaultValue={
-												bets?.find((bet) => bet.matchId === match.id)?.leftTeam
-											}
-										/>
-									) : (
-										<Typography>
-											{bets?.find((bet) => bet.matchId === match.id)
-												?.leftTeam || 0}
-										</Typography>
-									)}
-								</Grid>
-								<Grid item display="flex" alignItems="center">
-									<Typography>-</Typography>
-								</Grid>
-								<Grid item display="flex" alignItems="center">
-									{edit && index === match.id ? (
-										<TextField
-											{...register('rightScore', { required: true })}
-											style={{ width: '60px' }}
-											type="number"
-											inputProps={{ min: 0, max: 20 }}
-											defaultValue={
-												bets?.find((bet) => bet.matchId === match.id)?.rightTeam
-											}
-										></TextField>
-									) : (
-										<Typography marginRight="10px">
-											{bets?.find((bet) => bet.matchId === match.id)
-												?.rightTeam || 0}
-										</Typography>
-									)}
-									<Flag
-										code={match.rightFlag}
-										fallback={<span>Unknown</span>}
-										height="16"
-										style={{ margin: '10px' }}
-									/>
-									<Typography>{match.rightTeam}</Typography>
-								</Grid>
 								<Grid item>
-									{!edit && (
-										<FormButton
-											text="Edytuj"
-											sx={{ width: '20px', height: '25px' }}
-											onClick={() => {
-												setEdit(true);
-												setIndex(match.id);
-											}}
-										/>
-									)}
-									{edit && index === match.id && (
-										<FormButton
-											text="Obstaw"
-											sx={{ width: '20px', height: '25px' }}
-											type="submit"
-										/>
-									)}
+									<Grid container direction="column" rowGap={4}>
+										{matchGroup.map((match) => (
+											<form
+												key={match.id}
+												onSubmit={handleSubmit((data) =>
+													submitForm(data, match.id)
+												)}
+											>
+												<Grid
+													container
+													justifyContent="center"
+													alignItems="center"
+													sx={{
+														border: '1px solid black',
+														width: '500px',
+														height: '80px',
+													}}
+													direction="row"
+													spacing={2}
+												>
+													<Grid item display="flex" alignItems="center">
+														<Typography>{match.leftTeam}</Typography>
+														<Flag
+															code={match.leftFlag}
+															fallback={<span>Unknown</span>}
+															height="16"
+															style={{ margin: '10px' }}
+														/>
+														{edit && index === match.id ? (
+															<TextField
+																{...register('leftScore', { required: true })}
+																style={{ width: '60px' }}
+																type="number"
+																inputProps={{ min: 0, max: 20 }}
+																defaultValue={
+																	bets?.find((bet) => bet.matchId === match.id)
+																		?.leftTeam
+																}
+															/>
+														) : (
+															<Typography>
+																{loading ? (
+																	<CircularProgress />
+																) : (
+																	bets?.find((bet) => bet.matchId === match.id)
+																		?.leftTeam || 0
+																)}
+															</Typography>
+														)}
+													</Grid>
+													<Grid item display="flex" alignItems="center">
+														<Typography>-</Typography>
+													</Grid>
+													<Grid item display="flex" alignItems="center">
+														{edit && index === match.id ? (
+															<TextField
+																{...register('rightScore', { required: true })}
+																style={{ width: '60px' }}
+																type="number"
+																inputProps={{ min: 0, max: 20 }}
+																defaultValue={
+																	bets?.find((bet) => bet.matchId === match.id)
+																		?.rightTeam
+																}
+															></TextField>
+														) : (
+															<Typography marginRight="10px">
+																{loading ? (
+																	<CircularProgress />
+																) : (
+																	bets?.find((bet) => bet.matchId === match.id)
+																		?.rightTeam || 0
+																)}
+															</Typography>
+														)}
+														<Flag
+															code={match.rightFlag}
+															fallback={<span>Unknown</span>}
+															height="16"
+															style={{ margin: '10px' }}
+														/>
+														<Typography>{match.rightTeam}</Typography>
+													</Grid>
+													<Grid item>
+														{!edit && (
+															<FormButton
+																disable={isAfter(
+																	new Date(),
+																	new Date(match.dateTime)
+																)}
+																text="Edytuj"
+																sx={{ width: '20px', height: '25px' }}
+																onClick={() => {
+																	setEdit(true);
+																	setIndex(match.id);
+																}}
+															/>
+														)}
+														{edit && index === match.id && (
+															<FormButton
+																text="Obstaw"
+																sx={{ width: '20px', height: '25px' }}
+																type="submit"
+															/>
+														)}
+													</Grid>
+												</Grid>
+											</form>
+										))}
+									</Grid>
 								</Grid>
+								<Grid item>{matchGroup[0].date}</Grid>
 							</Grid>
-						</form>
+						</>
 					))}
 				</Grid>
 			</Container>
